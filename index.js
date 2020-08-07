961a7ebf929f6dd82ea924d670d444f6f1b61483
agee1 <orangemadcow@yahoo.com>
2020-08-06 21:19:42 -0700

    Add typeahead to search input

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var i = 0;
 function findRecipes(event){
 	console.log("what");
@@ -56,3 +58,36 @@ function findRandom(){
 		console.log(err);
 	});
 }
+
+var test = ["Mango", "Apple", "Orange"];
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+$(".form-control.typeahead").typeahead({
+	hint: true,
+	highlight: true,
+	minLength: 1
+},{
+	name: "ingredients",
+	source: substringMatcher(test)
+});
